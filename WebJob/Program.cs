bc2396d2dc04be7dc479c18a3b4fb0094a73b547
Colton Fussy <confusingboat@gmail.com>
2018-05-19 04:36:37 -0500

    comments or whatever

--- WebJob/Program.cs
+++ WebJob/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
+// If you change the assembly name (which I recommend),
+// don't forget to update the run.bat file
 namespace WebJob
 {
     public class Program
@@ -17,6 +19,8 @@ namespace WebJob
                 Console.WriteLine("Cancellation received");
                 host.Stop();
             });
+            // Assuming you want to get your shitty code up in the cloud ASAP
+            // so I've created this minimalistic config
             var jobHostConfig = new JobHostConfiguration
             {
                 HostId = Guid.NewGuid().ToString("N"),
@@ -31,8 +35,11 @@ namespace WebJob
         [NoAutomaticTrigger]
         public static async Task OneTimeTask()
         {
-            // Simulate some work
+            // Put your bullshit in here
+
+            // Simulate some work by making you wait
             await Task.Delay(2000);
+
             Console.WriteLine($"{nameof(OneTimeTask)} executed");
         }
     }
